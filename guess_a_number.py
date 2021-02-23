35f68b9f5329930866e2e169685406ebf52db465
komar666 <marcinkobiela1992@gmail.com>
2021-02-23 18:59:49 +0100

    Last code

---
+++ guess_a_number.py
@@ -0,0 +1,18 @@
+# Guess a number
+import random
+
+lot = random.randint(1, 10)
+print(lot)
+answer = input('Choose your number from 1 to 10:')
+
+while True:
+    if int(answer) == lot:
+        print('Bravo!!! You won!!')
+        break
+    elif int(answer) < lot:
+        print('The number u are looking for is bigger')
+        answer = input('Choose your number from 1 to 10:')
+    else:
+        print('The number u are looking for is smaller')
+        answer = input('Choose your number from 1 to 10:')
+    
